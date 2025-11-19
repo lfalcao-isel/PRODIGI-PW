fd93c01b4d51407c07c88b991a812b40737aed29
Luís Falcão <lfalcao@cc.isel.ipl.pt>
2025-11-19 18:57:29 +0000

    Lesson 01: JavaScript

---
+++ lessons/session1/hello.js
@@ -0,0 +1,38 @@
+console.log("Hello world")
+
+// JS
+let x = 20
+
+x = "20"
+
+x = [ 10, 11, 12, 13, 14, 15, 15, 14 ]
+
+x = x.slice(2,  5)
+
+
+
+let c =  { }
+
+c.disk = {
+    capacity: 256,
+    speed: 7200
+}
+
+let z = c.Disk
+
+console.log(c.disk.capacity)
+//..
+
+
+//.
+
+//.
+
+
+//.bRand
+
+
+console.log(z.capacity)
+
+
+
