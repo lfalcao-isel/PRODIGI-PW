c483e95c227d6bada6c376c18a5a5eea0408d062
fbfreitas <ffreitas@cc.isel.ipl.pt>
2025-11-28 19:02:31 +0000

    updated shooping-web-api.mjs

--- exercises/shooping-web-api.mjs
+++ exercises/shooping-web-api.mjs
@@ -24,25 +24,25 @@ const app = express();
 const PORT = 3000;
 
 // GET /lists?limit=number
-app.get('/lists', ) //1
+app.get('/lists', ) //1 add function
 // GET /lists/:id
-app.get(, getListById) //2
+app.get(, getListById) //2 add template path
 
 
 function getLists(req, resp){
-  const limit = //3
+  const limit = //3 get limit from query string
   if (limit && !isNaN(limit)) {
     resp.json(lists.slice(0, limit))
   } else {
-    //4
+    //4 response  - all lists in json
   }
 }
 
 function getListById(req, resp){
-     const id =  //5
+     const id =  //5 get id from path
      const list = lists.find(l => l.id == id)
      if (list) {
-        //6
+        //6 response - list in json
     } else {
         resp.status(404).json({ error: 'List not found' })
     }
