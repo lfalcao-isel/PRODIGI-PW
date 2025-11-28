0fc01c6fc245fe106cb2acc201dccd3e6c76e869
fbfreitas <ffreitas@cc.isel.ipl.pt>
2025-11-28 19:24:58 +0000

    demo-express

---
+++ exercises/demo-express/simple-express.mjs
@@ -0,0 +1,24 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/students', getStudents) //1 add function
+app.get('/students/:id', getStudentById) //2 add template path
+
+
+function getStudents(req, resp){
+  console.log("Query parameter name = " + req.query.name)
+  resp.json("getStudents")
+}
+
+function getStudentById(req, resp){
+    console.log("Path parameter id = " + req.params.id)
+    resp.json("getStudentId")
+}
+
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`)
+})
